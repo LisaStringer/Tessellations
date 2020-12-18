a1ed7aba698211e4764a140f381698e729a182c5
LisaStringer <53619224+LisaStringer@users.noreply.github.com>
2020-12-18 17:10:58 -0600

    Add files via upload

--- js/square_grid.js
+++ js/square_grid.js
@@ -15,14 +15,17 @@ var autumn_colors = ["ultramarine_green", "samba", "fired_brick", "amber_glow", 
 var autumn = [ultramarine_green, samba, "#692E2A", "#DD7A3F", "#D9CE52", "#0E4B81", "#6C254D", "#283057", "#645639", "#94949E"]
 var container = document.getElementById("container");
 var color_container = document.getElementById("color_container");
-
+var base_color = "#F2F2F2";
 var nav_bar = document.getElementById("nav_bar");
 var pentamino01 = ['00','01','02','11','21'];
+var pentamino01_flip = ['01', '11', '20', '21', '22'];
+
 var pentamino02 = ['00','01','02','03','13'];
 var pentamino03 = ['00','01','02','12','22'];
 var pentamino04 = ['01','10','11','12','13'];
 var pentamino05 = ['00','10','11','21','31'];
-var pentamino06 = ['01','03','10','11','21'];
+var pentamino06 = ['01','02','10','11','21'];
+var pentamino01_rotation = []
 var current_pattern = ['00','01','02','11','21'];
 function setColor(hex_code){
 	
@@ -58,12 +61,15 @@ function createPattern(){
 	var top_calc = 10;
 	
 	//calculate screen height and width
-	console.log("WIDTH: "+window.width +"HEIGHT: "+ window.height)
+	//console.log("WIDTH: "+window.width +"HEIGHT: "+ window.height)
 	var row_num = Math.floor((window.innerHeight-400)/70);
 	var col_num = Math.floor(window.innerWidth/70);
 	for (t = 0; t <= row_num; t++) {
-		
+		//make the names consistent 
+		if (t<10){t="0"+t}				 
 	for (n = 0; n <= col_num; n++) { 
+		//make the names consistent 
+		if (n<10){n="0"+n}
 		addSection("_"+t+"_"+n, top_calc, left_calc, "square");
 		//console.log("clip_"+t+"_"+n)
 		left_calc = left_calc + 67;
@@ -72,8 +78,6 @@ function createPattern(){
 		top_calc = top_calc + 67;
 		left_start = left_start;
 		left_calc = left_start;
-			
-		
 	}
 	
 	activateSVGPaths();
@@ -99,7 +103,7 @@ function createSample(sample_name){
 function addSection(id, top, left, start){
 	//console.log(id);
 	var tile = document.createElement("div");
-	tile.innerHTML = "<svg width='71' height='72' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' overflow='hidden'><defs><clipPath id='clip0'><rect x='65' y='74' width='71' height='72'/></clipPath></defs><g clip-path='url(#clip0)' transform='translate(-65 -74)' <path id='fill_"+id+"'><rect x='67.5001' y='76.5001' width='67' height='68' stroke='#4472C4' stroke-width='3' stroke-miterlimit='8' stroke-dasharray='3 3' fill='#F2F2F2'/></g></svg>";
+	tile.innerHTML = "<svg width='71' height='72' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' overflow='hidden'><defs><clipPath id='clip0'><rect x='65' y='74' width='71' height='72'/></clipPath></defs><g clip-path='url(#clip0)' transform='translate(-65 -74)' <path><rect id='fill"+id+"' x='67.5001' y='76.5001' width='67' height='68' stroke='#4472C4' stroke-width='3' stroke-miterlimit='8' stroke-dasharray='3 3' fill='#F2F2F2'/></g></svg>";
 	
 	var tile_hit = document.createElement("div");
 	tile_hit.id="hit"+id;
@@ -118,29 +122,39 @@ function addSection(id, top, left, start){
 }
 
 function showFill(fill_area_id){
-	console.log("showFill: "+fill_area_id.id)
+	//console.log("showFill: "+fill_area_id.id)
 	var pattern_path_id = fill_area_id.id;
-	var	top_num = pattern_path_id.substr(6, 1);
-	
-	top_num = Number(top_num)
+	if(pattern_path_id!=undefined){
+			
+	var	top_num = pattern_path_id.substr(5,2);
+	console.log("current FILL row number: "+top_num);
+	top_num = Number(top_num);
+		
 	var left_num = pattern_path_id.substr(8, 2);
+		console.log("current FILL col number: "+left_num);
 	left_num = Number(left_num)
-	
-	var triangle_type = pattern_path_id.substr(5, 1);
-	console.log("TOP: "+top_num+" LEFT: "+left_num+" TYPE: "+triangle_type)
-	fill_area_id.setAttributeNS(null, 'fill', curr_color);
-	
-	for (n = 0; n <= 5; n++) { 
-	var tile = document.getElementById("fill"+(top_num+1)+"_"+(left_num-1))
+	//console.log("MOUSE OVER: "+this.id);
+		
+	for(var i = 0; i < current_pattern.length; i++){
+		var tile_id = current_pattern[i];
+		var row_num = Number(tile_id.substr(0,1));
+		console.log("add this top number: "+row_num);
+		var col_num = Number(tile_id.substr(1,1));
+		console.log("add this left number: "+col_num);
+		//make names consistent
+		var row_id = top_num+row_num;
+		if (row_id <10){row_id="0"+row_id}
+		var col_id = left_num+col_num;
+		if (col_id <10){col_id="0"+col_id}
+		console.log("fill_"+row_id+"_"+col_id)
+		var td01 = document.getElementById("fill_"+row_id+"_"+col_id);
+		console.log(td01)
+	if(td01 != null){
 	td01.setAttributeNS(null, 'fill', curr_color);
-	var td02 = document.getElementById("fill"+(top_num+1)+"_"+left_num)
-	td02.setAttributeNS(null, 'fill', curr_color);
-	var td03 = document.getElementById("fill_D_"+(top_num+1)+"_"+(left_num+1))
-	td03.setAttributeNS(null, 'fill', curr_color);
-	var tu01 = document.getElementById("fill_U_"+(top_num+1)+"_"+(left_num))
-	tu01.setAttributeNS(null, 'fill', curr_color);
-	var tu02 = document.getElementById("fill_U_"+(top_num+1)+"_"+(left_num+1))
-	tu02.setAttributeNS(null, 'fill', curr_color);
+		//td01.style["background-color"] = curr_color;
+		
+	}			
+	}
 	}
 	
 	//var path = document.getElementById("fill"+path_substr)
@@ -159,7 +173,7 @@ function showFill(fill_area_id){
 		pattern_width = "1"
 		 }
 	*/
-	var control_color="#D9D9D9"
+	
 	//path_name.setAttributeNS(null, 'fill', control_color);
 /*	var lineclassElements = document.getElementsByClassName("line");
 	for(var i = 0; i < lineclassElements.length; i++){
@@ -173,31 +187,61 @@ function showFill(fill_area_id){
 	}*/
 }
 function setPattern(shape_name){
+	//get all buttons by attribute shape_button
+	var all_buttons = document.getElementsByClassName("shape_button");
 	
+	//reset all buttons
+	for(var i = 0; i < all_buttons.length; i++){
+		all_buttons[i].style.backgroundColor = "gainsboro";
+		all_buttons[i].style.opacity = "100%";
+	}
+	//highlight current shape
+	var current_button = document.getElementById(shape_name);
+	current_button.style.backgroundColor = "white";
+	current_button.style.opacity = "25%";
 	current_pattern = eval(shape_name);
 	var sample_window = document.getElementById("sample_window");
-	sample_window.innerHTML="<img src='images/"+shape_name+".svg'/>"
+	sample_window.innerHTML="<img class='sample_image' src='images/"+shape_name+".svg'/>"
+	
+	
 }
+function transformPattern(transform_method){
+	var change_button = document.getElementById(change_name);
+	if (change_name == "flip_up"){
+		change_button.innerHTML="<img src='images/flip_down.svg'>"}
+}
+
 function mouseOverEffect() {
   //fill_area_id.classList.add("shape-highlight");
 	
 	if(this!=undefined){
 			var pattern_path_id = this.id;
-	var	top_num = pattern_path_id.substr(6, 1);
-	
-	top_num = Number(top_num)
-	var left_num = pattern_path_id.substr(8, 2);
+	var	top_num = pattern_path_id.substr(4,2);
+	//console.log("current row number: "+top_num);
+	top_num = Number(top_num);
+		
+	var left_num = pattern_path_id.substr(7, 2);
+		//console.log("current col number: "+left_num);
 	left_num = Number(left_num)
-	console.log("MOUSE OVER: "+this.id);
+	//console.log("MOUSE OVER: "+this.id);
 		
 	for(var i = 0; i < current_pattern.length; i++){
 		var tile_id = current_pattern[i];
-		var row_num = Number(tile_id.substr(1,1));
-		var col_num = Number(tile_id.substr(1,2));
-		console.log("fill_"+(top_num+row_num)+"_"+(left_num+col_num))
-		var td01 = document.getElementById("fill_"+(top_num+row_num)+"_"+(left_num+col_num))
+		var row_num = Number(tile_id.substr(0,1));
+		//console.log("add this top number: "+row_num);
+		var col_num = Number(tile_id.substr(1,1));
+		//console.log("add this left number: "+col_num);
+		//make names consistent
+		var row_id = top_num+row_num;
+		if (row_id <10){row_id="0"+row_id}
+		var col_id = left_num+col_num;
+		if (col_id <10){col_id="0"+col_id}
+		//console.log("hit_"+row_id+"_"+col_id)
+		var td01 = document.getElementById("hit_"+row_id+"_"+col_id);
+		//console.log(td01)
 	if(td01 != null){
-	td01.setAttributeNS(null, 'fill', "#4472C4");
+	//td01.setAttributeNS(null, 'background-color', curr_color);
+		td01.style["background-color"] = curr_color;
 	}			
 	}
 	}
@@ -207,39 +251,37 @@ function mouseOverEffect() {
 	
 
 function mouseOutEffect() {
-	console.log("MOUSE OUt")
-  //this.classList.remove("shape-highlight");
-		console.log("MOUSE OVER: "+this.id)
-	var pattern_path_id = this.id;
-	var	top_num = pattern_path_id.substr(6, 1);
-	
-	top_num = Number(top_num)
-	var left_num = pattern_path_id.substr(8, 2);
+	//console.log("MOUSE OUt")
+ if(this!=undefined){
+			var pattern_path_id = this.id;
+	var	top_num = pattern_path_id.substr(4,2);
+	//console.log("current row number: "+top_num);
+	top_num = Number(top_num);
+		
+	var left_num = pattern_path_id.substr(7, 2);
+		//console.log("current col number: "+left_num);
 	left_num = Number(left_num)
-	
-	var triangle_type = pattern_path_id.substr(5, 1);
-	console.log("TOP: "+top_num+" LEFT: "+left_num+" TYPE: "+triangle_type)
-	//if U calculate from top
-	
-	var td01 = document.getElementById("fill_D_"+(top_num+1)+"_"+(left_num-1))
+	//console.log("MOUSE OVER: "+this.id);
+		
+	for(var i = 0; i < current_pattern.length; i++){
+		var tile_id = current_pattern[i];
+		var row_num = Number(tile_id.substr(0,1));
+		//console.log("add this top number: "+row_num);
+		var col_num = Number(tile_id.substr(1,1));
+		//console.log("add this left number: "+col_num);
+		//make names consistent
+		var row_id = top_num+row_num;
+		if (row_id <10){row_id="0"+row_id}
+		var col_id = left_num+col_num;
+		if (col_id <10){col_id="0"+col_id}
+		//console.log("hit_"+row_id+"_"+col_id)
+		var td01 = document.getElementById("hit_"+row_id+"_"+col_id);
+		//console.log(td01)
 	if(td01 != null){
-	td01.setAttributeNS(null, 'fill', "#ffffff");
+	//td01.setAttributeNS(null, 'background-color', curr_color);
+		td01.style["background-color"] = "transparent";
+	}			
 	}
-	var td02 = document.getElementById("fill_D_"+(top_num+1)+"_"+left_num)
-	if(td01 != null){
-	td01.setAttributeNS(null, 'fill', "#ffffff");
-	}
-	var td03 = document.getElementById("fill_D_"+(top_num+1)+"_"+(left_num+1))
-	if(td03 != null){
-	td03.setAttributeNS(null, 'fill', "#ffffff");
-	}
-	var tu01 = document.getElementById("fill_U_"+(top_num+1)+"_"+(left_num-1))
-	if(tu01 != null){
-	tu01.setAttributeNS(null, 'fill', "#ffffff");
-	}
-	var tu02 = document.getElementById("fill_U_"+(top_num+1)+"_"+(left_num))
-	if(tu02 != null){
-	tu02.setAttributeNS(null, 'fill', "#ffffff");
 	}
 }
 function activateSVGPaths(){
@@ -250,7 +292,8 @@ function activateSVGPaths(){
 		//lineclassElements[i].setAttributeNS(null, 'stroke', '#4472C4');
 		var hit_area_id = lineclassElements[i].id;
 		var fill_area_id = "fill"+hit_area_id.substr(3, 7);
-		//console.log("HIT AREA ID: "+fill_area_id)
+		
+		//console.log("FILL AREA ID: "+fill_area_id)
 		//var fill_area=document.getElementById(fill_area_id)
 		//lineclassElements[i].setAttributeNS(null, 'cursor', "hand");
 		lineclassElements[i].setAttributeNS(null, 'onclick',"showFill("+fill_area_id+")");
